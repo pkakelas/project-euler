b06033169b0de12ae1c2498c75d49ef870730a28
pkakelas <pkakelas@gmail.com>
2014-02-13 23:31:04 +0200

    Coding style

--- problem-2/problem-2.php
+++ problem-2/problem-2.php
@@ -1,16 +1,16 @@
 <?php 		
-	$sum=2;	
-	$a=1;
-	$b=2 ;
-	$c=3;
-	while ($c < 4000000){
-		$c=$a+$b;
-        $a=$b;
-		$b=$c;
-		$rest = fmod($c,2);
-         if ($rest==0){
-			$sum=$sum+$c ;
+	$sum = 2;	
+	$a = 1;
+	$b = 2 ;
+	$c = 3;
+	while ( $c < 4000000 ) {
+		$c = $a + $b;
+        $a = $b;
+		$b = $c;
+		$rest = fmod( $c, 2 );
+         if ( $rest == 0 ) {
+			$sum += $c;
 		}
 	}
-	echo $sum ;
+	echo $sum;
 ?>
