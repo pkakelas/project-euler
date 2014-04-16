3d3287e685bf4b8a2e604c306789b4c6b30433a0
pkakelas <pkakelas@gmail.com>
2014-04-16 16:57:38 +0300

    Add problem 3 in python

--- problem-3/problem-3.py
+++ problem-3/problem-3.py
@@ -2,10 +2,10 @@ import eratosthenes
 
 factors = []
 num = 600851475143 
-primes = eratosthenes.primes( 100 )
+primes = eratosthenes.primes( 10000 )
 for prime in primes:
     while num % prime == 0:
         num = num / prime
         factors.append( prime )
-print factors[ -1 ]
+print factors 
 
