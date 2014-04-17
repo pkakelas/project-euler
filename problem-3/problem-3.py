531e75115db393d5bd3111de7ea261509386281f
pkakelas <pkakelas@gmail.com>
2014-04-17 18:16:57 +0300

    corrections

--- problem-3/problem-3.py
+++ problem-3/problem-3.py
@@ -4,9 +4,9 @@ import primes
 
 factors = []
 num = 600851475143 
-primes = primes.primes( 10000 )
+primes = primes.primes(10000)
 for prime in primes:
     while num % prime == 0:
         num = num / prime
-        factors.append( prime )
+        factors.append(prime)
 print factors[-1] 
