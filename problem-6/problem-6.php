09953aa4fec715ae90cd80a50fbb219b1e2017ce
pkakelas <pkakelas@gmail.com>
2014-04-17 18:27:14 +0300

    solve problem 6 in python and correct the php solution

--- problem-6/problem-6.php
+++ problem-6/problem-6.php
@@ -1,11 +1,10 @@
-<?
+<?php
     $sum1 = $sum2 = 0;
     for ( $i = 0; $i < 101; $i++ ) {
-        $res = $i * $i;
-        $sum1 = $sum1 + $res;
+        $sum1  += $i * $i;
     }
     for ( $i = 0; $i < 101; $i++ ) {
-         $sum2 = $sum2 + $i;
+         $sum2 += $i;
     }
     $sum2 = $sum2 * $sum2;
     echo $sum2 - $sum1;
