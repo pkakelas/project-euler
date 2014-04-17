1a076bad0214c01615bbf4b67a337b9534c18354
pkakelas <pkakelas@gmail.com>
2014-04-17 19:01:07 +0300

    solve problem 7 in python

--- helpers/primes.py
+++ helpers/primes.py
@@ -1,13 +1,14 @@
 def primes( limit ):
-    primes = [ 2 ]
-    candidate = 3
+    primes = [2, 3]
+    candidate = primes[ -1 ] 
     for candidate in range(5, limit + 1, 2):
         isPrime = True
         for witness in primes:
             if witness * witness > candidate:
-                if candidate % witness == 0:
-                    isPrime = False
-                    break
+                break
+            if candidate % witness == 0:
+                isPrime = False
+                break
         if isPrime:
             primes.append( candidate )
     return primes
