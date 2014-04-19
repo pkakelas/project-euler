2b275ae4754387ea441163b3a4757c6c1cd8208b
pkakelas <pkakelas@gmail.com>
2014-04-19 20:05:51 +0300

    optimize problem 12

--- problem-12/problem-12.py
+++ problem-12/problem-12.py
@@ -2,10 +2,11 @@ import math
 counter = 1 
 
 while True:
-    sum = divCount = 0
+    sum = 0
+    divCount = 1
     for i in range( 1, counter + 1):
         sum += i 
-    for div in range(1, sum + 1): 
+    for div in range(1, sum / 2 + 1): 
         if sum % div == 0:
             divCount += 1
     counter += 1
