7e457f7006448e003e66808f67822e349dc6438e
pkakelas <pkakelas@gmail.com>
2014-04-20 20:46:37 +0300

    solve problem 18 in python

--- problem-12/problem-12.py
+++ problem-12/problem-12.py
@@ -1,15 +1,22 @@
 import math
 counter = 1 
 
+def divisor(n):
+    large_divisors = []
+    for i in xrange(1, int(math.sqrt(n) + 1)):
+        if n % i is 0:
+            yield i
+            if i is not n / i:
+                large_divisors.insert(0, n / i)
+    for divisor in large_divisors:
+        yield divisor
+
 while True:
-    sum = 0
-    divCount = 1
-    for i in range( 1, counter + 1):
-        sum += i 
-    for div in range(1, sum / 2 + 1): 
-        if sum % div == 0:
-            divCount += 1
-    counter += 1
-    if divCount > 500: 
+    sum = counter * (counter + 1) / 2 
+    if len(list(divisor(sum))) > 500: 
         print sum
         break
+    counter += 1
+    print counter
+
+print sum
