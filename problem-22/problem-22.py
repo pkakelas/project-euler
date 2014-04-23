b7045447dbdb46ac4a483689b12750338ef35a7c
pkakelas <pkakelas@gmail.com>
2014-04-23 19:21:47 +0300

    solve #22 in python

--- problem-22/problem-22.py
+++ problem-22/problem-22.py
@@ -2,15 +2,20 @@ file = open('problem-22/names.in', 'r')
 names = file.read()
 strings = names.split(",")
 
-letters = [ 'A', 'B', 'C', 'D', 'R', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-names = scores = []
+letters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+names = []
+sum = 0
 
-for name in strings:
+for name in sorted(strings):
     name = name.replace('"', '')
     if '\n' in name:
         name = name.replace('\n', '')
     names.append(name)
 
-print names
+for name in names:
+    nameScore = 0 
+    for letter in name:
+        nameScore += letters.index(letter) + 1
+    sum += nameScore * (names.index(name) + 1)
 
-
+print sum
