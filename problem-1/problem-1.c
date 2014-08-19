7b4909f26757bffb8c87d7f3cf9e375cb97b1813
Dimitris Lamprinos <pkakelas@gmail.com>
2014-08-19 15:07:04 +0300

    correction

--- problem-1/problem-1.c
+++ problem-1/problem-1.c
@@ -5,7 +5,7 @@ int main() {
 	int counter;
     int sum = 0;
 	
-    for ( counter = 1; $counter < 1000; counter++ ) {
+    for ( counter = 1; counter < 1000; counter++ ) {
          if ( counter % 3 == 0 || counter % 5 == 0 ) {
             sum += counter;
          }
